4734717de65e1415bae47ca12ad1d72bef489d42
GaboBalbin <gabobalbin01@gmail.com>
2023-11-18 14:51:15 +0800

    Made major operation except divison work

--- script.js
+++ script.js
@@ -6,84 +6,117 @@ const otherButtons = document.querySelectorAll('.other-button');
 let buffer = ''
 
 
-let num1 = 0;
-let num2 = 0;
-let operation = 0;
+let num1 = NaN;
+let num2 = NaN;
+let operator = NaN;
+let answer = NaN;
 
 
-// my version
-// for (let i = 0; i<buttons.length; i++){
-//     buttons[i].addEventListener('click', e=>{console.log(e.target.textContent)})
-// }
 
 
 // Give every number and decimal a button
 buttons.forEach(button => {
-    button.addEventListener('click', e => {
-        buffer += e.target.innerText
+    button.addEventListener('click', main => {
+        
+        buffer += main.target.innerText
         result.innerText = buffer; // Updates the results
-        num1 = buffer
+
+
+
+
+        
         
     })
 })
 
+
+
 // Give other buttons (AC +/- % / *...) functionality
 otherButtons.forEach(otherButton => {
-    otherButton.addEventListener('click', functionality)
+    otherButton.addEventListener('click', other => {
+        
+        // so if num1 is not equal to NaN (has value) and num2 is equal to 
+        // Nan (no value), then put the result in num2. Else, put it in num1
+
+
+        
+        if (other.target.innerText === "AC"){
+            num1 = NaN;
+            num2 = NaN;
+            operator = NaN;
+            answer = NaN;
+            result.innerText = 0
+            buffer = '';
+        }
+        else if (!isNaN(num1) && isNaN(num2)){
+            num2 = result.innerText
+            buffer = '';
+
+            console.log(operator)
+            operate(num1, operator, num2)
+            operator = other.target.innerText
+            // Functions have a weird way of changing variables. Change it after
+            num1 = answer
+            num2 = NaN
+        }
+
+        else if (isNaN(num1) && isNaN(num2)){
+            num1 = result.innerText
+            operator = other.target.innerText
+            buffer = '';
+        }
+
+
+
+
+    })
     })
 
 
 
 
 
-// operation
-function functionality(e){
-    if (e.target.getAttribute('data-value') === 'AC'){
-        result.textContent = 0;
-        buffer = ''
-        num1 = 0;
-        num2 = 0;
-    }
-    else if (e.target.getAttribute('data-value') === '+/-'){
-        // ?
-    }
-    else if (e.target.getAttribute('data-value') === '%'){
-        // percentage
-    }
-    else if (e.target.getAttribute('data-value') === '/'){
-        // divide
-    }
-    else if (e.target.getAttribute('data-value') === '*'){
-        // multiply
-    }
-    else if (e.target.getAttribute('data-value') === '-'){
-        // subtract
-    }
-    else if (e.target.getAttribute('data-value') === '+'){
-        // add
-        result.innerText = Number(num2) + Number(num1)
-        console.log(num1)
-        console.log(num2)
-
-        num2 = result.innerText
 
 
-        
-        buffer=''
-
-        
-        
 
 
-        
+
+function operate(num1, operator, num2){
+    if (operator === "+"){
+        answer = Number(num1) + Number(num2);
+
     }
-    else if (e.target.getAttribute('data-value') === '='){
-        // results
+    else if (operator === "-"){
+        answer = Number(num1) - Number(num2);
     }
+    else if (operator === "*"){
+        answer = Number(num1) * Number(num2);
+    }
+    else if (operator === "/"){
+        answer = Number(num1) / Number(num2);
+    }
+    result.innerText = answer;
+    buffer = '';
 }
 
 
 
-// function to add, subtract
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// my version to add all the buttons
+// for (let i = 0; i<buttons.length; i++){
+//     buttons[i].addEventListener('click', e=>{console.log(e.target.textContent)})
+// }
